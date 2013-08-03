926360e5cf2f5beaaf80d1de7dd1b93b8d7d1bce
Chinthaka Godawita <chinthaka.godawita@demonzmedia.com>
2013-08-03 13:15:31 +1000

    Now handles releases as embedded documents and added mongo

--- test.rb
+++ test.rb
@@ -1,4 +1,5 @@
 require 'feedzirra'
+require 'mongo'
 require 'logging'
 
 # Build custom URL: 'Scene+P2P', 'DVDRip' (or greater), only new (last 3 years).
@@ -12,6 +13,29 @@ logger.add_appenders(
   Logging.appenders.stdout
 )
 logger.level = :debug
+
+# Init MongoDB.
+include Mongo
+db = MongoClient.new('localhost', 27017).db('vcdq')
+movies_collection = db.collection('movies')
+
+# class Movie
+#   include MongoMapper::Document
+
+#   key :title, String
+#   key :year, Integer
+
+#   many :releases
+# end
+
+# # Mongo model for a movie release
+# class Release
+#   include MongoMapper::EmbeddedDocument
+
+#   key :quality, String
+#   key :url, String
+#   key :date, String
+# end
 
 # Get movie info for the title
 # @see http://en.wikipedia.org/wiki/Standard_(warez)#Naming
@@ -79,26 +103,76 @@ end
 # response = RSS::Parser.parse(VCDQ_RSS_URL, false)
 # feed_parsed = response.channel.items
 feed = Feedzirra::Feed.fetch_raw(vcdq_rss_url)
+puts feed
 feed_parsed = Feedzirra::Feed.parse(feed)
 
 i = 0
-feed_parsed.entries.each do |movie|
-  movie_title_parts = movie.title.split('.')
+parsed_movies = Hash.new
+feed_parsed.entries.each do |movie_item|
+  movie_title_parts = movie_item.title.split('.')
 
-  # puts movie
-  logger.debug("Processing title: #{movie.title}")
+  # puts movie_item
+  logger.debug("Processing title: #{movie_item.title}")
 
-  movie_info = get_movie_info(movie_title_parts, movie.categories)
+  movie_info = get_movie_info(movie_title_parts, movie_item.categories)
 
   # If we couldn't parse any info, just log this incident and move on
   # @TODO: reformat to throw exception instead
   next if movie_info.nil?
 
   # We know that the fourth category is always the quality, save it for later.
-  movie_info[:quality] = movie.categories[3]
+  movie_info[:quality] = movie_item.categories[3]
 
-  logger.debug("Parsed movie info: #{movie_info}")
-  # puts movie_info
+  # Create a movie.
+  movie = {
+    title: movie_info[:title],
+    title_lower: movie_info[:title].downcase,
+    year: movie_info[:year],
+    i: i,
+    releases: Array.new
+  }
+  release = {
+    quality: movie_info[:quality],
+    url: movie_item.url,
+    date: movie_item.published,
+    i: i
+  }
+
+  movie_exists = movies_collection.find_one({
+    title_lower: movie[:title_lower],
+    year: movie[:year]
+  });
+
+  if !movie_exists
+    # Do an insert of this movie.
+    movies_collection.insert(movie)
+  end
+
+  # Do an update/insert of this release.
+  logger.debug("trying for #{movie[:title_lower]}")
+  # test = movies_collection.find_one({
+  #   title_lower: movie[:title_lower],
+  #   year: movie[:year],
+  #   'releases.url' => {
+  #     '$ne' => movie_item.url
+  #   }
+  # });
+  # logger.debug(" THE TEST ")
+  # logger.debug(test)
+
+  movies_collection.update({
+    title_lower: movie[:title_lower],
+    year: movie[:year],
+    'releases.url' => {
+      '$ne' => movie_item.url
+    }
+  }, {
+    '$push' => {
+      releases: release
+    }
+  })
+
+  # logger.debug("Parsed movie info: #{movie_info}")
 
   i += 1
 end
